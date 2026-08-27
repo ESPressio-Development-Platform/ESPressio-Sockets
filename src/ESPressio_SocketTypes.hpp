193357db340b1a4809a1fa0c093d1dec7d5a5fec
Simon J Stuart <lakraven@gmail.com>
2026-08-27 18:33:21 +0200

    refactor(sockets): remove Arduino and FreeRTOS types from public socket concepts (#32)

--- src/ESPressio_SocketTypes.hpp
+++ src/ESPressio_SocketTypes.hpp
@@ -1,58 +1,61 @@
 #pragma once
 
+#include <array>
 #include <cstddef>
 #include <cstdint>
 
-#include <Arduino.h>
-#include <IPAddress.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
+#include <ESPressio_Platform.hpp>
 
 namespace ESPressio::Sockets {
 
-struct SocketEndpoint {
-    IPAddress Address;
-    uint16_t Port = 0;
+struct IPv4Address {
+    std::array<uint8_t, 4> Octets{};
 
-    SocketEndpoint() = default;
+    constexpr IPv4Address() = default;
+    constexpr IPv4Address(uint8_t a, uint8_t b, uint8_t c, uint8_t d)
+        : Octets{{a, b, c, d}} {}
 
-    SocketEndpoint(
-        const IPAddress& address,
-        uint16_t port
-    ) :
-        Address(address),
-        Port(port) {
+    constexpr bool IsZero() const noexcept {
+        return Octets[0] == 0 && Octets[1] == 0 &&
+               Octets[2] == 0 && Octets[3] == 0;
     }
 
-    bool IsValid() const noexcept {
-        return
-            Port != 0 &&
-            static_cast<uint32_t>(Address) != 0;
+    constexpr bool operator==(const IPv4Address& other) const noexcept {
+        return Octets == other.Octets;
     }
 
-    bool operator==(
-        const SocketEndpoint& other
-    ) const noexcept {
-        return
-            Address == other.Address &&
-            Port == other.Port;
-    }
-
-    bool operator!=(
-        const SocketEndpoint& other
-    ) const noexcept {
+    constexpr bool operator!=(const IPv4Address& other) const noexcept {
         return !(*this == other);
     }
 };
 
+struct SocketEndpoint {
+    IPv4Address Address;
+    uint16_t Port = 0;
+
+    SocketEndpoint() = default;
+    constexpr SocketEndpoint(const IPv4Address& address, uint16_t port)
+        : Address(address), Port(port) {}
+
+    constexpr bool IsValid() const noexcept {
+        return Port != 0 && !Address.IsZero();
+    }
+
+    constexpr bool operator==(const SocketEndpoint& other) const noexcept {
+        return Address == other.Address && Port == other.Port;
+    }
+
+    constexpr bool operator!=(const SocketEndpoint& other) const noexcept {
+        return !(*this == other);
+    }
+};
 
 struct SocketWorkerConfig {
     uint32_t StackSize = 4096;
-    UBaseType_t Priority = 2;
-    BaseType_t Core = tskNO_AFFINITY;
+    uint32_t Priority = 2;
+    System::ProcessorAffinity Affinity = System::ProcessorAffinity::Any();
     uint32_t IdleDelayMilliseconds = 2;
 };
-
 
 #ifndef ESPRESSIO_SOCKETS_MAX_EVENT_PACKET_SIZE
     #define ESPRESSIO_SOCKETS_MAX_EVENT_PACKET_SIZE 65536
@@ -66,4 +69,4 @@ struct SocketWorkerConfig {
     #define ESPRESSIO_SOCKETS_MAX_TCP_CLIENTS 8
 #endif
 
-}
+} // namespace ESPressio::Sockets
